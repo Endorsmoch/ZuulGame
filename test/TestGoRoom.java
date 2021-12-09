c1e9ca0b44f2795d0e77041bc117938923835979
Rodrigo Urtecho <A16003872@alumnos.uady.mx>
2021-12-08 22:26:44 -0600

    PruebasCompletas

---
+++ test/TestGoRoom.java
@@ -0,0 +1,46 @@
+package test;
+
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import game.Command;
+import game.GameMovement;
+import game.MapFileReader;
+import game.NullRoom;
+import game.Parser;
+import game.Room;
+import game.RoomList;
+
+public class TestGoRoom {
+
+	MapFileReader reader;
+	Command command;
+	Parser parser;
+	Room beforeRoom;
+	Room nextRoom;
+	
+	@Before
+	public void preconditionGoRoom() {
+		beforeRoom = new Room("Sala");
+		nextRoom = new Room("Cocina");
+		beforeRoom.setExit("east", nextRoom);
+		beforeRoom.setExit("north", new NullRoom());
+		beforeRoom.setExit("south", new NullRoom());
+		beforeRoom.setExit("west", new NullRoom());
+		nextRoom.setExit("west", new NullRoom());
+		nextRoom.setExit("east", new NullRoom());
+		nextRoom.setExit("north", new NullRoom());
+		nextRoom.setExit("south", new NullRoom());
+		command = new Command("ir", "este");
+	}
+	@Test
+	public void goRoomTestUnos() {
+		GameMovement mover = new GameMovement();
+		Room currentRoom = mover.goRoom(command,  beforeRoom);
+		assertEquals(currentRoom, beforeRoom);
+	}
+
+}
