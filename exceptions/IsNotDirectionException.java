fbbbd82fb215b09a7de54e164962a5f4e3f2a129
Rodrigo Urtecho <A16003872@alumnos.uady.mx>
2021-12-09 18:39:36 -0600

    newExceptionND

---
+++ exceptions/IsNotDirectionException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class IsNotDirectionException extends RuntimeException {
+	public IsNotDirectionException(String message){
+        super(message);
+	}
+}
--- game/GameMovement.java
+++ game/GameMovement.java
@@ -1,6 +1,7 @@
 package game;
 
 import exceptions.SecondParameterException;
+import exceptions.IsNotDirectionException;
 
 public class GameMovement {
 	
@@ -28,26 +29,32 @@ public class GameMovement {
 	
 	private Room searchRoom(String direction, Room currentRoom) {
 		Room roomFound = new NullRoom();
-    	switch(direction) {
-        case "norte":
-        	roomFound = currentRoom.getNorthExit();
-        	break;
-        case "este":
-        	roomFound = currentRoom.getEastExit();
-        	break;
-        case "sur":
-        	roomFound = currentRoom.getSouthExit();
-        	break;
-        case "oeste":
-        	roomFound = currentRoom.getWestExit();
-        	break;
-        }
-
-    	return roomFound;
+		try {	
+	    	switch(direction) {
+	        case "norte":
+	        	roomFound = currentRoom.getNorthExit();
+	        	break;
+	        case "este":
+	        	roomFound = currentRoom.getEastExit();
+	        	break;
+	        case "sur":
+	        	roomFound = currentRoom.getSouthExit();
+	        	break;
+	        case "oeste":
+	        	roomFound = currentRoom.getWestExit();
+	        	break;
+	        default:
+	        	throw new IsNotDirectionException("No es una dirección válida");
+	        }
+		}catch(IsNotDirectionException e) {
+			System.out.println(e.getMessage());
+		}
+		return roomFound;
 	}
 	
 	private void printCurrentRoomWithExits(Room currentRoom) {
     	System.out.println(currentRoom.getDescription());
         System.out.print("Salidas: " + currentRoom.getStringExits() + "\n");
     }
+	
 }
